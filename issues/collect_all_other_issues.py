c11de2270b58102f8a97100215f3107b6caadf8d
Rohit Mandge <Rohit Mandge>
2016-04-28 16:23:41 -0400

    Generated graphs for collect_all_other_issues

--- issues/collect_all_other_issues.py
+++ issues/collect_all_other_issues.py
@@ -4,6 +4,7 @@ import json
 import re,datetime
 import sys, os
 import csv
+import matplotlib.pyplot as plt
  
 class L():
   "Anonymous container"
@@ -26,73 +27,105 @@ def secs(d0):
   delta = d - epoch
   return delta.total_seconds()
 
-def get_all_issues(file_name):
-  csvfile = file(file_name,'rb')
-  reader = csv.reader(csvfile)
-  f.write('\n')
-  txt = "=========== # "+ os.path.splitext(file_name)[0] + " ISSUES # ==========="
-  txt = str(txt)
-  print(txt)
-  f.write(txt)
-  f.write('\n')
-  t = []
-  labels = set([])
-  count_not_closed = 0
-  count_not_labelled = 0
-  count_not_milestones = 0
-  count_not_in_time = 0
-  count_not_description = 0
-  for line in reader:
-    [number,title,description,state,creator,create_at,labels,milestonedue,last_update] = line
-    if state != 'closed':
-      count_not_closed += 1
-    if labels == '[]':
-      count_not_labelled += 1
-    if milestonedue == '-1':
-      count_not_milestones += 1
-    if description == '':
-      count_not_description += 1
-    if int(float(milestonedue)) > 0 and int(float(last_update)) > int(float(milestonedue)):
-      if state == 'closed':
-        count_not_in_time += 1
+def plot_graph(projects,count,desc):
+  plt.bar(range(len(projects)),count,width=0.50, align='center', color = 'rgbymc')
+  plt.xticks(range(len(projects)), projects)
+  plt.ylabel(desc)
+  plt.xlabel('Projects')
+  locs, projects = plt.xticks()
+  plt.setp(projects)
+  plt.tick_params(axis='both', which='minor', labelsize=10)
+  plt.tick_params(axis='both', which='major', labelsize=10)
+  plt.savefig(desc+'.png')
+  plt.clf()
+
+def get_all_issues():
+  i = 1
+  not_closed = []
+  not_labelled = []
+  not_milestones = []
+  not_in_time = []
+  not_description = []
+  while i <= 3:
+  	  file_name = 'project'+str(i)+'.csv'
+	  csvfile = file(file_name,'rb')
+	  reader = csv.reader(csvfile)
+	  f.write('\n')
+	  txt = "=========== # "+ os.path.splitext(file_name)[0] + " ISSUES # ==========="
+	  txt = str(txt)
+	  print(txt)
+	  f.write(txt)
+	  f.write('\n')
+	  t = []
+	  labels = set([])
+	  count_not_closed = 0
+	  count_not_labelled = 0
+	  count_not_milestones = 0
+	  count_not_in_time = 0
+	  count_not_description = 0
+	  for line in reader:
+	    [number,title,description,state,creator,create_at,labels,milestonedue,last_update] = line
+	    if state != 'closed':
+	      count_not_closed += 1
+	    if labels == '[]':
+	      count_not_labelled += 1
+	    if milestonedue == '-1':
+	      count_not_milestones += 1
+	    if description == '':
+	      count_not_description += 1
+	    if int(float(milestonedue)) > 0 and int(float(last_update)) > int(float(milestonedue)):
+	      if state == 'closed':
+	        count_not_in_time += 1
+
+	  txt = "ISSUES NOT CLOSED"
+	  print(txt)
+	  print(count_not_closed)
+	  not_closed.append(count_not_closed)
+	  f.write(txt + " : "+ str(count_not_closed))
+	  f.write('\n')
+
+	  txt = "ISSUES WITHOUT LABELS"
+	  print(txt)
+	  print(count_not_labelled)
+	  not_labelled.append(count_not_labelled)
+	  f.write(txt + " : "+ str(count_not_labelled))
+	  f.write('\n')
+
+	  txt = "ISSUES WITHOUT MILESTONES"
+	  print(txt)
+	  print(count_not_milestones)
+	  not_milestones.append(count_not_milestones)
+	  f.write(txt + " : "+ str(count_not_milestones))
+	  f.write('\n')
+
+	  txt = "OVERDUE ISSUES"
+	  print(txt)
+	  print(count_not_in_time)
+	  not_in_time.append(count_not_in_time)
+	  f.write(txt + " : "+ str(count_not_in_time))
+	  f.write('\n')
+
+	  txt = "ISSUES WITHOUT DESCRIPTION"
+	  print(txt)
+	  print(count_not_description)
+	  not_description.append(count_not_description)
+	  f.write(txt + " : "+ str(count_not_description))
+	  f.write('\n')
+	  
+	  csvfile.close()
+	  i += 1
   
-  txt = "ISSUES NOT CLOSED"
-  print(txt)
-  print(count_not_closed)
-  f.write(txt + " : "+ str(count_not_closed))
-  f.write('\n')
+  projects = ['project1', 'project2', 'project3']
 
-  txt = "ISSUES WITHOUT LABELS"
-  print(txt)
-  print(count_not_labelled)
-  f.write(txt + " : "+ str(count_not_labelled))
-  f.write('\n')
-
-  txt = "ISSUES WITHOUT MILESTONES"
-  print(txt)
-  print(count_not_milestones)
-  f.write(txt + " : "+ str(count_not_milestones))
-  f.write('\n')
-
-  txt = "OVERDUE ISSUES"
-  print(txt)
-  print(count_not_in_time)
-  f.write(txt + " : "+ str(count_not_in_time))
-  f.write('\n')
-
-  txt = "ISSUES WITHOUT DESCRIPTION"
-  print(txt)
-  print(count_not_description)
-  f.write(txt + " : "+ str(count_not_description))
-  f.write('\n')
-  
-  csvfile.close()
+  plot_graph(projects,not_closed,'issues_not_closed')
+  plot_graph(projects,not_labelled,'issues_without_labels')
+  plot_graph(projects,not_milestones,'issues_without_milestones')
+  plot_graph(projects,not_in_time,'issues_overdue')
+  plot_graph(projects,not_description,'issues_without_description')
 
 f = open('issue_summary.txt','w')
 f.truncate()
 
-get_all_issues('project1.csv')
-get_all_issues('project2.csv')
-get_all_issues('project3.csv')
+get_all_issues()
 
 f.close()
